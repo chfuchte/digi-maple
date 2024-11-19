85c84dad92ccf71f99e4d9af163f3aa1a724729d
Christian <christian0511@gmx.de>
2024-11-19 22:21:46 +0100

    add tailwind + eslint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
-    devtools: { enabled: true },
+    app: {
+        head: {
+            bodyAttrs: {
+                class: "dark",
+            }
+        }
+    },
+    css: ["~/assets/css/global.css"],
+    modules: ['@vueuse/nuxt', "@nuxtjs/tailwindcss", "@nuxt/eslint"],
+    tailwindcss: {
+        exposeConfig: true,
+        viewer: false,
+        configPath: "./tailwind.config",
+        cssPath: "./assets/css/global.css",
+    },
+    $development: {
+        devtools: { enabled: true },
+        tailwindcss: {
+            viewer: true,
+        },
+    },
+    ssr: true,
 });
