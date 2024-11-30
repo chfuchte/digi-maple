46658b2d4444a39a58b33311544b7415c8aa6e30
kurwjan <kurwjann@gmail.com>
2024-11-30 01:20:18 +0100

    Reformat code

--- frontend/components/DebugBar.vue
+++ frontend/components/DebugBar.vue
@@ -5,17 +5,17 @@
 </template>
 
 <style scoped>
-    div {
-        background: repeating-linear-gradient(
-            45deg,
-            #ff4e4e,
-            #ff4e4e 10px,
-            #af1616 10px,
-            #af1616 20px
-        );
-    }
+div {
+    background: repeating-linear-gradient(
+        45deg,
+        #ff4e4e,
+        #ff4e4e 10px,
+        #af1616 10px,
+        #af1616 20px
+    );
+}
 
-    h6 {
-        text-shadow: 0 0 3px #000;
-    }
+h6 {
+    text-shadow: 0 0 3px #000;
+}
 </style>
--- frontend/components/Map.client.vue
+++ frontend/components/Map.client.vue
@@ -20,46 +20,46 @@ const zoomOutDisabled = ref(true);
 let leafletObject = ref<Map | null>(null);
 
 const onMapReady = (map: Map) => {
-  leafletObject.value = map;
-}
+    leafletObject.value = map;
+};
 
 const zoomEvent = (zoomLevel: number) => {
-  zoomInDisabled.value = zoomLevel == leafletObject.value?.getMaxZoom();
-  zoomOutDisabled.value = zoomLevel == leafletObject.value?.getMinZoom();
-}
+    zoomInDisabled.value = zoomLevel == leafletObject.value?.getMaxZoom();
+    zoomOutDisabled.value = zoomLevel == leafletObject.value?.getMinZoom();
+};
 
 const bounds = latLngBounds([0, 0], [props.width!, props.height!]);
 </script>
 
 <template>
     <div class="flex flex-col">
-      <TitleBar>{{ props.name }}</TitleBar>
-      <LMap
-          ref="map"
-          :zoom="-2"
-          :center="[bounds.getCenter().lat, bounds.getCenter().lng]"
-          :crs="CRS.Simple"
-          :min-zoom="-2"
-          :max-zoom="2"
-          :options="{
+        <TitleBar>{{ props.name }}</TitleBar>
+        <LMap
+            ref="map"
+            :zoom="-2"
+            :center="[bounds.getCenter().lat, bounds.getCenter().lng]"
+            :crs="CRS.Simple"
+            :min-zoom="-2"
+            :max-zoom="2"
+            :options="{
                 zoomControl: false,
                 attributionControl: false,
             }"
-          @ready="onMapReady"
-          @update:zoom="zoomEvent"
-          class="flex-grow">
-        <LControl position="bottomleft">
-          <MapZoom
-              @zoom-in="leafletObject?.zoomIn();"
-              @zoom-out="leafletObject?.zoomOut();"
-              :disable-zoom-in="zoomInDisabled"
-              :disable-zoom-out="zoomOutDisabled"
-          />
-        </LControl>
-        <LImageOverlay :url="props.image!" :bounds />
-        <LMarker v-for="marker in markers" :key="marker.name ?? ''" :lat-lng="marker">
-          <LPopup :content="marker.name ?? ''" />
-        </LMarker>
-      </LMap>
+            @ready="onMapReady"
+            @update:zoom="zoomEvent"
+            class="flex-grow">
+            <LControl position="bottomleft">
+                <MapZoom
+                    @zoom-in="leafletObject?.zoomIn();"
+                    @zoom-out="leafletObject?.zoomOut();"
+                    :disable-zoom-in="zoomInDisabled"
+                    :disable-zoom-out="zoomOutDisabled"
+                />
+            </LControl>
+            <LImageOverlay :url="props.image!" :bounds />
+            <LMarker v-for="marker in markers" :key="marker.name ?? ''" :lat-lng="marker">
+                <LPopup :content="marker.name ?? ''" />
+            </LMarker>
+        </LMap>
     </div>
 </template>
