b3dbd0288d63366a8e9c89ab9611342dc58d71bd
Christian <christian0511@gmx.de>
2024-12-01 12:51:05 +0100

    small fixes

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,6 @@ import sys
 
 try:
     from fastapi import FastAPI
-    from pydantic import BaseModel
     import uvicorn
 
 except:
@@ -16,14 +15,10 @@ except:
 
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
-
 #Import project modules
 import simple_module
 
-
-
 app = FastAPI()
-
 
 #Communication with the server (Simple example)
 
@@ -31,16 +26,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, x: Union[str, None] = None):
     return {"item_id": item_id, "x": x}
 
-
-
 # Main (create local server)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    
-
+    uvicorn.run(app, host="0.0.0.0", port=8080)
