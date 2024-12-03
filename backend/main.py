f9b24ac3d93fce8be2637c5a84ee245c016f2744
kurwjan <kurwjann@gmail.com>
2024-12-03 21:10:33 +0100

    Revert "del backend"
    This reverts commit c9db4a5e31117d22e0814455ef2dac5b0eb02b4b.

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from typing import Union
+import subprocess
+import sys
+
+#import FaskAPI and pydantic
+#Install the packages if needed
+
+try:
+    from fastapi import FastAPI
+    import uvicorn
+
+except:
+    print("Warning: FastAPI and it's dependencies are not installed", file=sys.stderr)
+    print("Installing requirements...")
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
+#Import project modules
+import simple_module
+
+app = FastAPI()
+
+#Communication with the server (Simple example)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, x: Union[str, None] = None):
+    return {"item_id": item_id, "x": x}
+
+# Main (create local server)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
