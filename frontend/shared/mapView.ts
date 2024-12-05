4bea65973ae2d4c41e15922cc497388c52c0a503
Christian <christian0511@gmx.de>
2024-12-05 18:02:59 +0100

    types

---
+++ frontend/shared/mapView.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+const mapViewSchema = z.object({
+    name: z.string(),
+    author: z.string(),
+    imgUrl: z.string().url(),
+    imgWidth: z.number(),
+    imgHeight: z.number(),
+    markers: z.array(
+        z.object({
+            id: z.string(),
+            x: z.number(),
+            y: z.number(),
+            display: z.object({
+                title: z.string(),
+                description: z.string(),
+                markerType: z.enum(["default"]),
+            }),
+        }),
+    ),
+});
+
+type MapView = z.infer<typeof mapViewSchema>;
+type MapMarker = MapView["markers"][number];
+
+export { type MapView, type MapMarker, mapViewSchema };
