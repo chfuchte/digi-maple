b8266b4f5e97239da0c0361809684bbb5a3858aa
Christian <christian0511@gmx.de>
2024-12-28 13:35:15 +0100

    small stuff

--- frontend/components/TitleHeader.vue
+++ frontend/components/TitleHeader.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-
 const { getUser } = useCurrentUserStore()
 const user = computed(() => getUser())
 
 const colorMode = useColorMode()
-
-console.log(colorMode.value)
 
 const toggleColorMode = () => {
     colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light';
@@ -14,7 +11,7 @@ const toggleColorMode = () => {
 </script>
 
 <template>
-    <header class="flex h-12 items-center justify-between px-4 shadow-sm">
+    <header class="flex h-12 items-center justify-between px-4 border-b border-secondary">
         <NuxtLink to="/" class="my-2 flex items-center gap-3">
             <div class="flex items-center gap-1">
                 <LucideMap :size="30" />
@@ -22,26 +19,28 @@ const toggleColorMode = () => {
             </div>
         </NuxtLink>
 
-        <NuxtLink to="/auth/logout" v-if="user">
-            <Button variant="secondary">Abmelden</Button>
-        </NuxtLink>
-        <NuxtLink to="/auth" v-else>
-            <Button variant="secondary">Anmelden</Button>
-        </NuxtLink>
+        <div class="flex items-center gap-2">
+            <NuxtLink to="/auth/logout" v-if="user">
+                <Button variant="link">Abmelden</Button>
+            </NuxtLink>
+            <NuxtLink to="/auth" v-else>
+                <Button variant="link">Anmelden</Button>
+            </NuxtLink>
 
-        <DropdownMenu>
-            <DropdownMenuTrigger as-child>
-                <Button variant="ghost">
-                    <LucideEllipsisVertical />
-                    <span class="sr-only">Einstellungen</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem @click="toggleColorMode">
-                    <LucideSunMoon />
-                    <span>Toggle Dark Mode</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger as-child>
+                    <Button variant="ghost">
+                        <LucideEllipsisVertical />
+                        <span class="sr-only">Einstellungen</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem @click="toggleColorMode" class="flex items-center justify-end gap-2">
+                        <LucideSunMoon />
+                        <span>Toggle Dark Mode</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
     </header>
 </template>
