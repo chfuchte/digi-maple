d4a8d67b9f06944eb8d623caaf3c6f3b443a337a
Christian <christian0511@gmx.de>
2025-05-08 17:25:33 +0200

    fix dashboard

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/com
 import { Input } from "@/components/ui/input";
 import { MagnifyingGlassIcon } from "@radix-icons/vue";
 import { apiSearchMaps } from "@/queries/maps";
-import { Fragment, ref } from "vue";
+import { ref } from "vue";
 import type { Map } from "@/typings/map";
 import { toast } from "vue-sonner";
 
@@ -34,38 +34,49 @@ const handleSearchInput = (e: { target: { value: string } }) => {
 
 <template>
     <Layout>
-        <div class="my-4 w-full border-0 px-4 relative items-center">
+        <div class="relative my-4 w-full items-center border-0 px-4">
             <Input @input="handleSearchInput" id="search" type="text" placeholder="Suche..." class="w-full pl-10" />
             <span class="absolute inset-y-0 start-0 flex items-center justify-center px-6">
                 <MagnifyingGlassIcon class="size-6 text-muted-foreground" />
             </span>
         </div>
         <div class="w-full">
-            <CardHeader v-if="searchResults.length === 0">
-                <CardTitle class="text-2xl font-semibold">Keine Karten gefunden</CardTitle>
-                <CardDescription>
-                    Es konnte keine Karten gefunden werden, die deiner Anfrage entsprechen.
-                </CardDescription>
-            </CardHeader>
-            <Fragment v-else>
+            <div v-if="searchResults.length > 0">
                 <CardHeader>
                     <CardTitle class="text-2xl font-semibold">Suchergebnisse</CardTitle>
                     <CardDescription>Hier sind die Suchergebnisse für deine Anfrage.</CardDescription>
                 </CardHeader>
-                <div class="flex gap-4 px-4 flex-wrap flex-row w-full">
-                    <RouterLink class="max-w-80 w-3/4" v-for="map in searchResults" :to="`/maps/${map.id}`"
+                <div class="flex w-full flex-row flex-wrap gap-4 px-4">
+                    <RouterLink
+                        class="w-3/4 max-w-80"
+                        v-for="map in searchResults"
+                        :to="`/maps/${map.id}`"
                         :key="map.id">
                         <Card class="w-full">
                             <CardHeader>
                                 <CardTitle>{{ map.name }}</CardTitle>
                             </CardHeader>
                             <CardContent class="flex justify-center">
-                                <img :src="map.imgUrl" alt="Map Image" class="h-auto w-full" />
+                                <CardContent class="flex h-[300px] items-center justify-center bg-background">
+                                    <div
+                                        v-if="map.imgHeight && map.imgWidth"
+                                        :style="`background-image: url(${map.imgUrl}); background-repeat: no-repeat; background-size: cover; background-position: center center;`"
+                                        class="h-full w-full" />
+                                    <p v-else class="text-center text-2xl font-semibold">
+                                        Bitte Kartenbild hochgeladen
+                                    </p>
+                                </CardContent>
                             </CardContent>
                         </Card>
                     </RouterLink>
                 </div>
-            </Fragment>
+            </div>
+            <CardHeader v-else>
+                <CardTitle class="text-2xl font-semibold">Keine Karten gefunden</CardTitle>
+                <CardDescription>
+                    Es konnte keine Karten gefunden werden, die deiner Anfrage entsprechen.
+                </CardDescription>
+            </CardHeader>
         </div>
     </Layout>
 </template>
